dc092a9620b7e41c0b99729e4c93bd2c3e557f12
Pablo López Martín <lopezmartinpablo@gmail.com>
2022-02-28 14:00:00 +0100

    Añadidos los @entity y BBDD funcionando

--- src/main/java/es/ucm/fdi/iw/model/Reparacion.java
+++ src/main/java/es/ucm/fdi/iw/model/Reparacion.java
@@ -1,5 +1,23 @@
 package es.ucm.fdi.iw.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Reparacion {
-    
+    public Reparacion(){
+
+    }
+
+    private long id; 
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    public long getId() { 
+        return id; 
+    }
+    public void setId(long id) { this.id = id; }
 }
