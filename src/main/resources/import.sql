99c6f7b45c6e8a82ebd73f5c23e116a4f2f20c37
Pablo López Martín <lopezmartinpablo@gmail.com>
2022-03-14 20:31:07 +0100

    Funcionan los roles de la vista de admin

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,18 @@
 -- insert admin (username a, password aa)
-INSERT INTO IWUser (id, enabled, roles, username, password)
-VALUES (1, TRUE, 'ADMIN,USER', 'a',
-    '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
-INSERT INTO IWUser (id, enabled, roles, username, password)
-VALUES (2, TRUE, 'USER', 'b',
-    '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
+--INSERT INTO IWUser (id, enabled, roles, username, password)
+--VALUES (1, TRUE, 'ADMIN,USER', 'a',
+  --  '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
+--INSERT INTO IWUser (id, enabled, roles, username, password)
+--VALUES (2, TRUE, 'USER', 'b',
+  --  '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
+    
+INSERT INTO "IWUSER" VALUES
+(1, TRUE, 'Nombre1', 'Apellido1', '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W', 'ADMIN,USER,CLIENTE', 'a'),
+(2, TRUE, 'Nombre2', 'Apellido2', '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W', 'USER,EMPLEADO', 'b');     
 
 
 -- start id numbering from a value that is larger than any assigned above
 ALTER SEQUENCE "PUBLIC"."GEN" RESTART WITH 1024;
+
+
+
