b27335eaf06035a47aa1b1bb8a6cd0eb974c77f6
Pablo López Martín <lopezmartinpablo@gmail.com>
2022-03-20 18:25:56 +0100

    Ahora el texto de index.html, se coge de la BD y es personalizable por el administrador TOTO: en un futuro currrarse mas la personalizacion de index.html

---
+++ src/main/java/es/ucm/fdi/iw/model/TextoTaller.java
@@ -0,0 +1,23 @@
+package es.ucm.fdi.iw.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+
+@Entity
+@Data
+public class TextoTaller {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private long id; 
+
+    private String texto;
+
+
+
+}
